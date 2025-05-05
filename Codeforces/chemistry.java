baf1008c3589061d7426105750e6a2d006259ec9
wajid-pallet <wajid.a@palletnow.co>
2025-05-05 23:38:55 +0530

    chemistry

---
+++ Codeforces/chemistry.java
@@ -0,0 +1,43 @@
+package Codeforces;
+
+import java.util.*;
+
+public class chemistry {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            String s = sc.next();
+
+            if (k > n) {
+                System.out.println("NO");
+                continue;
+            }
+
+            int[] freq = new int[26];
+
+            for (char ch : s.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+
+            int oddCnt = 0;
+
+            for (int num : freq) {
+                if (num % 2 != 0) {
+                    oddCnt++;
+                }
+            }
+
+            if (oddCnt > k + 1) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+
+        sc.close(); 
+    }
+}
