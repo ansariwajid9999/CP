fec4d3632b31cb9a37bfa2c4e7d4143efbff69b0
wajid-pallet <wajid.a@palletnow.co>
2025-05-07 17:57:33 +0530

    divisor and zero added

--- Codeforces/jellyfish.java
+++ Codeforces/jellyfish.java
@@ -2,7 +2,7 @@ package Codeforces;
 
 import java.util.*;
 
-public class jel {
+public class jellyfish {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
